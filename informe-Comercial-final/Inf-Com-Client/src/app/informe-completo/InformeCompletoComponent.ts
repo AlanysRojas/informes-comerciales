32f869f65d6b7186f21057c0b08171e871d4044c
AlanysRojas <afv.rojas@yavirac.edu.ec>
2019-10-15 14:13:44 -0500

    decimales

--- informe-Comercial-final/Inf-Com-Client/src/app/informe-completo/InformeCompletoComponent.ts
+++ informe-Comercial-final/Inf-Com-Client/src/app/informe-completo/InformeCompletoComponent.ts
@@ -218,7 +218,7 @@ export class InformeCompletoComponent implements OnInit {
     detallOperacionesVencidasCompleto: string;
 
 
-//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 
     balanceGeneralFecha: string;
